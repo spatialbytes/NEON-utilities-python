d01f2d49cb82919a916b625e22d9610627d3e212
Bridget Hass <bhass@battelleecology.org>
2023-09-14 14:23:00 -0600

    add more code up to checking download size, add get_file_urls and convert_byte_size functions
    will clean up tons of comments later

--- nupy/src/neonutilities/by_file_aop.py
+++ nupy/src/neonutilities/by_file_aop.py
@@ -8,6 +8,135 @@ Created on Wed Sep 13 13:10:18 2023
 
 import re
 from get_api import get_api
+import pandas as pd
+
+##############################################################################################
+# ' @title Get and store the file names, S3 URLs, file size, and download status (default = 0) in a data frame
+
+# ' @author
+# ' Claire Lunch \email{clunch@battelleecology.org}
+# ' Christine Laney \email{claney@battelleecology.org}
+
+# ' @description Used to generate a data frame of available AOP files.
+# '
+# ' @param m.urls The monthly API URL for the AOP files
+# ' @param token User specific API token (generated within neon.datascience user accounts)
+
+# ' @return A dataframe comprised of file names, S3 URLs, file size, and download status (default = 0)
+
+# ' @references
+# ' License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+# Changelog and author contributions / copyrights
+#   Claire Lunch (2018-02-19): original creation
+#   Christine Laney (2018-03-05): Added functionality to get new list of URLs if the old ones expire, during the download stream.
+
+##############################################################################################
+
+# get and stash the file names, google cloud URLs, file size, and download status (default = 0) in a data frame
+
+# def list_urls_by_product(self, dpid):
+#     """
+#     list_urls_by_product lists all the available api url for a given data product id (dpid)
+#     """
+#     product_url = self.construct_product_url(dpid)
+#     # print(product_url)
+#     r = self.make_request(product_url)
+#     data_urls = []
+#     for i in range(len(r['data']['siteCodes'])):
+#         data_urls_temp = r['data']['siteCodes'][i]['availableDataUrls']
+#         data_urls.extend(data_urls_temp)
+
+#     return data_urls
+
+
+def convert_byte_size(size_bytes):
+
+    # if size_bytes < 10**3:
+    #     size_printout = ('Download size:', round(size_bytes, 1), 'bytes')
+    if 10**3 < size_bytes < 10**6:
+        size_kb = round(size_bytes/(10**3), 2)
+        size_read = f'{size_kb} KB'
+    elif 10**6 < size_bytes < 10**9:
+        size_mb = round(size_bytes/(10**6), 1)
+        size_read = f'{size_mb} MB'
+        # print('Download size:', round(size/(10**6), 2), 'MB')
+    elif 10**9 < size_bytes < 10**12:
+        size_gb = round(size_bytes/(10**9), 1)
+        size_read = f'{size_gb} GB'
+        # print('Download size:', round(size/(10**9), 2), 'GB')
+    else:
+        size_tb = round(size_bytes/(10**12), 1)
+        size_read = f'{size_tb} TB'
+        # print('Download size:', round(size/(10**12), 2), 'TB')
+    return size_read
+
+
+def get_file_urls(urls, token=None):
+    # url_messages = []
+    # releases = []
+
+    for url in urls:
+        req = get_api(api_url=url, token=token)
+        if not req:
+            print(
+                f"Data file retrieval failed. Check NEON data portal for outage alerts.")
+
+        # get release info
+        release = req.json()['data']['release']
+
+        file_url_dict = req.json()['data']['files']
+        file_url_df = pd.DataFrame(data=file_url_dict)
+        # drop md5 and crc32 columns, which are all NaNs
+        file_url_df.drop(columns=['md5', 'crc32'], inplace=True)
+
+    return file_url_df, release
+
+# getFileUrls < - function(m.urls, token=NA){
+#     url.messages < - character()
+#     file.urls < - c(NA, NA, NA)
+#     releases < - character()
+#     for(i in 1: length(m.urls)) {
+
+#         tmp < - getAPI(apiURL=m.urls[i], token=token)
+
+#         if(tmp$status_code != 200) {
+#             message(paste("Data file retrieval failed with code ", tmp$status_code,
+#                           ". Check NEON data portal for outage alerts.", sep=""))
+#             return(invisible())
+#         }
+
+#         tmp.files < - jsonlite: : fromJSON(httr: : content(tmp, as='text', encoding='UTF-8'),
+#                                           simplifyDataFrame=TRUE, flatten=TRUE)
+
+#         # check for no files
+#         if(length(tmp.files$data$files) == 0) {
+#             url.messages < - c(url.messages, paste("No files found for site", tmp.files$data$siteCode,
+#                                                    "and year", tmp.files$data$month, sep=" "))
+#             next
+#         }
+
+#         # get release info
+#         releases < - c(releases, tmp.files$data$release)
+
+#         file.urls < - rbind(file.urls, cbind(tmp.files$data$files$name,
+#                                              tmp.files$data$files$url,
+#                                              tmp.files$data$files$size))
+
+#     }
+
+#     # get size info
+#     file.urls < - data.frame(file.urls, row.names=NULL)
+#     colnames(file.urls) < - c("name", "URL", "size")
+#     file.urls$URL < - as.character(file.urls$URL)
+#     file.urls$name < - as.character(file.urls$name)
+
+#     if(length(url.messages) > 0){writeLines(url.messages)}
+#     file.urls < - file.urls[-1, ]
+#     release < - unique(releases)
+#     return(list(file.urls, release))
+
+# }
 
 
 def by_file_aop(dpid,
@@ -134,7 +263,7 @@ def by_file_aop(dpid,
 #   }
 
     request_dict = request.json()
-    # error message if data are not from AOP
+    # error message if dpid is not an AOP data product
     if request_dict['data']['productScienceTeamAbbr'] != 'AOP':
         print(
             f'{dpid} is not a remote sensing product. Use zipsByProduct()')
@@ -143,7 +272,28 @@ def by_file_aop(dpid,
 #     stop(paste(dpID, "is not a remote sensing product. Use zipsByProduct()"))
 #   }
 
-#   # check for sites that are flown under the flight box of a different site
+    # check for sites that are flown under the flight box of a different site
+    # this next section pulls from the shared_flights.csv file (could also use
+    # a Python pickle, but csv is more transparent on Git)
+    # still need to determine the best place to put it, link below recommends
+    # "Files which are to be used by your installed library should usually be
+    # placed inside of the Python module directory itself"
+    # https://python-packaging.readthedocs.io/en/latest/non-code-files.html
+
+    shared_flights_df = pd.read_csv('./shared_flights.csv')
+    # .to_dict(orient='tight',index=False)
+
+    shared_flights_dict = shared_flights_df.set_index(
+        ['site'])['flightSite'].to_dict()
+    if site in shared_flights_dict:
+        flightSite = shared_flights_dict[site]
+        if site in ['TREE', 'CHEQ', 'KONA', 'DCFS']:
+            print(
+                f'{site} is part of the flight box for {flightSite}. Downloading data from {flightSite}.')
+        else:
+            print(f'{site} is an aquatic site and is sometimes included in the flight box for {flightSite}. Aquatic sites are not always included in the flight coverage every year. \nDownloading data from {flightSite}. Check data to confirm coverage of {site}.')
+        site = flightSite
+
 #   if(site %in% shared_flights$site) {
 #     flightSite <- shared_flights$flightSite[which(shared_flights$site==site)]
 #     if(site %in% c('TREE','CHEQ','KONA','DCFS')) {
@@ -157,15 +307,36 @@ def by_file_aop(dpid,
 #     site <- flightSite
 #   }
 
-#   # get the urls for months with data available, and subset to site
+    # get the urls for months with data available, and subset to site
+    site_info = next(
+        item for item in request_dict['data']['siteCodes'] if item["siteCode"] == site)
+    site_urls = site_info['availableDataUrls']
+    # print(site_urls)
 #   month.urls <- unlist(avail$data$siteCodes$availableDataUrls)
 #   month.urls <- month.urls[grep(paste(site, year, sep="/"), month.urls)]
 
-#   # error message if nothing is available
+    # error message if nothing is available
+    if len(site_urls) == 0:
+        print("There are no data available at the selected site and year.")
+
 #   if(length(month.urls)==0) {
 #     stop("There are no data at the selected site and year.")
 #   }
 
+    # get file url dataframe for the available month urls
+    file_url_df, release = get_file_urls(site_urls, token=token)
+
+    # get the number of files in the dataframe, if there are no files to download, return
+    num_files = len(file_url_df)
+    if num_files == 0:
+        print("No data files found.")
+        return
+
+    # get the total size of all the files found
+    download_size_bytes = file_url_df['size'].sum()
+    download_size = convert_byte_size(download_size_bytes)
+    #   downld.size <- sum(as.numeric(as.character(file.urls.current[[1]]$size)), na.rm=T)
+    #   downld.size.read <- convByteSize(downld.size)
 #   file.urls.current <- getFileUrls(month.urls, token = token)
 #   if(is.null(file.urls.current)) {
 #     message("No data files found.")
@@ -174,7 +345,12 @@ def by_file_aop(dpid,
 #   downld.size <- sum(as.numeric(as.character(file.urls.current[[1]]$size)), na.rm=T)
 #   downld.size.read <- convByteSize(downld.size)
 
-#   # ask user if they want to proceed
+    # ask user if they want to proceed
+    if check_size:
+        print('Download size:', download_size)
+        # if input(f"Continuing will download {num_files} totaling approximately {download_size}. Do you want to proceed? (y/n)") != "y":
+        #     print('Exiting by_file_aop')
+        #     return
 #   # can disable this with check.size=F
 #   if(check.size==TRUE) {
 #     resp <- readline(paste("Continuing will download ", nrow(file.urls.current[[1]]), " files totaling approximately ",
@@ -194,4 +370,4 @@ def by_file_aop(dpid,
 # releases: []
 # siteCodes: NoneType object
 
-    return request
+    return
