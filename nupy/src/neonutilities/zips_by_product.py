0a5091f41a83f8cef63baf24d9dcf6caeb9b7593
Claire Lunch <clunch@neoninc.org>
2024-01-31 15:30:03 -0700

    start zips_by_product
    not functional yet

---
+++ nupy/src/neonutilities/zips_by_product.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import get_api
+import re
+import sys
+
+def zips_by_product(dpID, site, startdate, enddate, package="basic", 
+                    release="current", include_provisional=False, 
+                    token=None, savepath=None):
+    """
+    Download product-site-month data package files from NEON.
+    
+    Parameters
+    --------
+    dpID: Data product identifier in the form DP#.#####.###
+    site: One or more 4-letter NEON site codes
+    package: Download package to access, either basic or expanded
+    startdate: Earliest date of data to download, in the form YYYY-MM
+    enddate: Latest date of data to download, in the form YYYY-MM
+    release: Data release to download. Defaults to the most recent release.
+    include_provisional: Should Provisional data be returned in the download? Defaults to False.
+    token: User specific API token (generated within neon.datascience user accounts). If omitted, download uses the public rate limit.
+    savepath: File path of location to save data.
+
+    Return
+    --------
+    A folder on the local drive containing the set of data package files meeting the input criteria.
+
+    Example
+    --------
+    Download water quality data from COMO (Como Creek) in 2018
+
+    >>> zips_by_product(dpID="DP1.20288.001",site="COMO",
+                        startdate="2018-01", enddate="2018-12",
+                        token=None, savepath="/mypath/Downloads")
+    
+    Created on Wed Jan 31 14:36:22 2024
+
+    @author: clunch
+    """
+
+    # error message if dpID is not formatted correctly
+    if not re.search(pattern="DP[1-4]{1}.[0-9]{5}.00[0-9]{1}", 
+                 string=dpID):
+        sys.exit(dpID+" is not a properly formatted data product ID. The correct format is DP#.#####.00#")
+    
+    # error message if package is not basic or expanded
+    if not package in ["basic","expanded"]:
+        sys.exit(package+" is not a valid package name. Package must be basic or expanded")
+        
+    # many more error messages and special handling needed here - see R package
+    
+    # query the /products endpoint for the product requested
+    if release=="current" or release=="PROVISIONAL":
+        prodreq = get_api(api_url="http://data.neonscience.org/api/v0/products/"+dpID,
+                          token=token)
+    else:
+        prodreq = get_api(api_url="http://data.neonscience.org/api/v0/products/"
+                          +dpID+"?release="+release, token=token)
+
+
