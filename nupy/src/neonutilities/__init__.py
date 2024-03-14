157e590c98088995f0159f86ce563ef8b63acd5b
Claire Lunch <clunch@neoninc.org>
2024-03-14 09:57:44 -0600

    add simple tests for get_citation()

--- nupy/src/neonutilities/__init__.py
+++ nupy/src/neonutilities/__init__.py
@@ -1,5 +1,6 @@
 from .citation import get_citation
 from .api_helpers import get_api
 from .api_helpers import get_zip_urls
-from .aop_download import by_file_aop, by_tile_aop
+from .aop_download import by_file_aop
+from .aop_download import by_tile_aop
 from .zips_by_product import zips_by_product
---
+++ nupy/tests/test_get_citation.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 14 Mar 2024
+
+@author: Claire Lunch
+
+Unit tests for get_citation()
+
+Mocking is not used here, tests access API resources.
+
+"""
+
+# import required packages
+import os
+from neonutilities import get_citation
+
+import pytest
+
+# read in token from os.environ
+token = os.environ.get("NEON_TOKEN")
+
+### Test a provisional and a release citation ###
+
+def test_get_citation_provisional():
+    """
+    Test that the get_citation() function returns the expected citation for provisional data
+    """
+    cit = get_citation(dpID="DP1.10003.001", release="PROVISIONAL")
+    citexp = '@misc{DP1.10003.001/provisional,\n  doi = {},\n  url = {https://data.neonscience.org/data-products/DP1.10003.001},\n  author = {{National Ecological Observatory Network (NEON)}},\n  language = {en},\n  title = {Breeding landbird point counts (DP1.10003.001)},\n  publisher = {National Ecological Observatory Network (NEON)},\n  year = {2024}\n}'
+    assert cit==citexp
+
+def test_get_citation_release():
+    """
+    Test that the get_citation() function returns the expected citation for a Release
+    """
+    cit = get_citation(dpID="DP1.10098.001", release="RELEASE-2023")
+    citexp = '@misc{https://doi.org/10.48443/73zn-k414,\n  doi = {10.48443/73ZN-K414},\n  url = {https://data.neonscience.org/data-products/DP1.10098.001/RELEASE-2023},\n  author = {{National Ecological Observatory Network (NEON)}},\n  keywords = {plant productivity, production, carbon cycle, biomass, vegetation, productivity, plants, trees, shrubs, lianas, saplings, net primary productivity (NPP), annual net primary productivity (ANPP), woody plants, vegetation structure, tree height, canopy height, vst},\n  language = {en},\n  title = {Vegetation structure (DP1.10098.001)},\n  publisher = {National Ecological Observatory Network (NEON)},\n  year = {2023}\n}\n'
+    assert cit==citexp
