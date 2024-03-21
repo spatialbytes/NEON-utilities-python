cb2de8254589e8d065f172bc8d2e6566cca8d569
Zach Nickerson <nickerson@battelleecology.org>
2024-03-21 14:37:51 -0400

    first version of unzip_zipfile_parallel done

--- nupy/src/neonutilities/unzip_and_stack.py
+++ nupy/src/neonutilities/unzip_and_stack.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import zipfile
+import platform
+import glob
+import re
+
+
+zippath = "C:/Users/nickerson/Downloads/NEON_sediment (6).zip"
+outpath = zippath[:-4]
+level = 'all'
+n_cores = 1
+
 def unzip_zipfile_parallel(zippath,
-                           outpath,# substr(zippath, 1, nchar(zippath)-4) << convert R code
+                           outpath=zippath[:-4],
                            level='all',
                            n_cores=1):
     """
@@ -11,10 +22,10 @@ def unzip_zipfile_parallel(zippath,
     
     Parameters
     --------
-    zip_path: The filepath of the input file.
-    out_path: The name of the folder to save unpacked files to.
-    level: Whether the unzipping should occur only for the 'top' zip file, or unzip 'all' recursively, or only files 'in' the folder specified.
-    n_cores: Number of cores to use for parallelization.
+    zippath: The filepath of the input file.
+    outpath: The name of the folder to save unpacked files to. Defaults to a directory with the same path as the input zip file.
+    level: Whether the unzipping should occur only for the 'top' zip file, or unzip 'all' recursively, or only files 'in' the folder specified. Defaults to 'all'.
+    n_cores: Number of cores to use for parallelization. Defaults to 1.
     
     Return
     --------
@@ -30,6 +41,49 @@ def unzip_zipfile_parallel(zippath,
     
     @author: Zachary Nickerson
     """    
+    
+    # Error handling on inputs
+    if not zippath[-4:] in ['.zip','.ZIP']:
+        print('Zip file read failed because zippath must be a file path to a compressed zip file (file type .zip or .ZIP).\n')
+        return
+    if not level in ['top','all','in']:
+        print('Unzipping failed because level must be one of the followin options: all, in, top.\n')
+        return
+    if not isinstance(n_cores, int):
+        print('Unzipping in parellel failed because n_cores must be an integer.\n')
+        return
+    
+    if level == 'all':
+        zip_ref = zipfile.ZipFile(zippath,'r')
+        tl = zip_ref.namelist()
+        
+        # Error handling for filepath character lengths
+        if any(len(x) > 260 for x in tl) and platform.system() == "Windows":
+            print('Longest filepath is ' + str(len(max(tl, key=len))) + ' characters long. Filepaths on Windows are limited to 260 characters. Move files closer to the root directory.')
+            return
+        if any(len(x) > 255 for x in tl) and platform.system() == "Darwin":
+            print('Longest filepath is ' + str(len(max(tl, key=len))) + ' characters long. Filepaths on Mac are limited to 255 characters. Move files closer to the root directory.')
+            return
+        
+        # Unzip file and get list of zips within
+        zip_ref.extractall(outpath)
+        zps = glob.glob(outpath+"/*.zip")
+        
+        # If there are more zips within parent zip file, unzip those as well
+        if type(zps) == list:
+            print('need an example to properly code this up.\n')
+        else:
+            zps = outpath
+        
+        return(zps)
+        
+# filepath=zps
+filepath=zippath
+savepath=None
+save_unzipped_files=False
+dpID=None
+package=None 
+n_cores=1
 
 def stack_by_table(filepath,
                    savepath=None, 
@@ -67,5 +121,32 @@ def stack_by_table(filepath,
     @author: Zachary Nickerson
     """  
     
+    # Is the filepath input a list, a zip file, or an upzipped file?
+    if type(filepath) == list:
+        folder = 'ls'
+        save_unzipped_files = False
+    else:
+        if filepath[-4:] in ['.zip','.ZIP']:
+            folder = False
+        else:
+            folder = True
+    
+    # Check whether data should be stacked
+    if folder == False:
+        zip_ref = zipfile.ZipFile(filepath,'r')
+        files = zip_ref.namelist()
+        pattern = re.compile('NEON.D[0-9]{2}.[A-Z]{4}.')
+        files = [s for s in files if pattern.match(s)]
     
     
+    #
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #
