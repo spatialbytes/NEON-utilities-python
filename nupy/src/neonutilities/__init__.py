7236d190ad5fc4b26450f407ef316e06d044d93e
Claire Lunch <clunch@neoninc.org>
2024-05-02 17:14:43 -0600

    modify read_table_neon

--- nupy/src/neonutilities/__init__.py
+++ nupy/src/neonutilities/__init__.py
@@ -2,4 +2,5 @@ from .citation import get_citation
 from .aop_download import by_file_aop, by_tile_aop
 from .tabular_download import zips_by_product
 from .get_issue_log import get_issue_log
+from .read_table_neon import read_table_neon
 from . import helper_mods
