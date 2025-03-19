7813aae520e7014c3c61926883e277cf6eab8aea
Bridget Hass <bhass@battelleecology.org>
2025-03-19 13:52:19 -0600

    remove commented out import

--- src/neonutilities/__init__.py
+++ src/neonutilities/__init__.py
@@ -1,5 +1,4 @@
 from .citation import get_citation
-#from .aop_download import by_file_aop, by_tile_aop
 from .aop_download import by_file_aop, by_tile_aop, list_available_dates, get_aop_tile_extents
 from .tabular_download import zips_by_product
 from .get_issue_log import get_issue_log
